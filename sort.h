de170eeac2847d6310ecc5c081966f8ed43c509a
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-04-11 16:58:42 +0000

    task2

--- sort.h
+++ sort.h
@@ -6,7 +6,6 @@
 
 /**
  * struct listint_s - Doubly linked list node
- *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
@@ -25,5 +24,6 @@ void print_list(const listint_t *list);
 /* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif /* SORT_H */
